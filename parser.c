f3638469f84fa2b309dc5e0ea15718fcbe55260e
anaio <anaionita.fly@gmail.com>
2025-11-28 14:43:11 +0200

    s9

--- parser.c
+++ parser.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include "lexer.h"
+#include "ad.h"
 
 int iTk; //index token curent
 Token *consumed; 
@@ -25,31 +26,64 @@ bool consume(int code){
     return false;
 }
 
+void initBuiltins() {
+    
+    
+    Symbol *s;
+
+    s = addSymbol("puti", KIND_FN);
+    s->type = TYPE_INT;
+    addFnArg(s, "x")->type = TYPE_INT;
+
+    s = addSymbol("putr", KIND_FN);
+    s->type = TYPE_INT;
+    addFnArg(s, "x")->type = TYPE_REAL;
+
+    s = addSymbol("puts", KIND_FN);
+    s->type = TYPE_INT;
+    addFnArg(s, "x")->type = TYPE_STR;
+}
+
+
 /*** program ::= ( defVar | defFunc | block )* FINISH ***/
 bool defVar();
 bool defFunc();
 bool block();
 
 bool program(){
+    addDomain();   // <-- domeniul global
+    initBuiltins();  // adaugă builtin-urile în acest domeniu
+
     for(;;){
         if(defVar()) continue;
         if(defFunc()) continue;
         if(block()) continue;
         break;
     }
+
     if(!consume(FINISH)) tkerr("missing FINISH");
+
+    delDomain(); // <-- închidere domeniu global
     return true;
 }
 
 /*** defVar ::= VAR ID COLON baseType SEMICOLON ***/
 bool baseType();
 
-bool defVar(){   // var x: int;
+bool defVar(){
     int start=iTk;
     if(consume(VAR)){
         if(consume(ID)){
+            const char *name = consumed->text;
+            Symbol *s = searchInCurrentDomain(name);
+            if(s) tkerr("symbol redefinition: %s", name);
+            s = addSymbol(name, KIND_VAR);
+            s->local = crtFn != NULL;
+
             if(consume(COLON)){
                 if(baseType()){
+                    s->type = ret.type;
+
                     if(consume(SEMICOLON)) return true;
                     tkerr("missing ; in variable definition");
                 }
@@ -60,37 +94,64 @@ bool defVar(){   // var x: int;
     return false;
 }
 
+
 /*** baseType ::= TYPE_INT | TYPE_REAL | TYPE_STR ***/
 bool baseType(){
-    return consume(TYPE_INT) || consume(TYPE_REAL) || consume(TYPE_STR);  
+    if(consume(TYPE_INT)){
+        ret.type = TYPE_INT;
+        return true;
+    }
+    if(consume(TYPE_REAL)){
+        ret.type = TYPE_REAL;
+        return true;
+    }
+    if(consume(TYPE_STR)){
+        ret.type = TYPE_STR;
+        return true;
+    }
+    return false;
 }
+
 
 /*** defFunc ::= FUNCTION ID LPAR funcParams? RPAR COLON baseType defVar* block END ***/
 bool funcParams();  //   a: int, b: real;
 
-bool defFunc(){   // function f (a: int, b: real) : int  
-                    //      return a;
-                    //  end
+bool defFunc(){
     int start=iTk;
     if(consume(FUNCTION)){
         if(!consume(ID)) tkerr("missing function name");
+
+        const char *name = consumed->text;
+        Symbol *s = searchInCurrentDomain(name);
+        if(s) tkerr("symbol redefinition: %s", name);
+        crtFn = addSymbol(name, KIND_FN);
+        crtFn->args = NULL;
+
+        addDomain(); // domeniul local al funcției
+
         if(!consume(LPAR)) tkerr("missing ( in function");
-        funcParams(); // optional
+        funcParams();
         if(!consume(RPAR)) tkerr("missing ) in function");
 
         if(!consume(COLON)) tkerr("missing : in function");
         if(!baseType()) tkerr("missing return type");
+        crtFn->type = ret.type;
 
         while(defVar());
         if(!block()) tkerr("missing function body");
 
         if(!consume(END)) tkerr("missing END");
+        
+        void delDomain();
+
+        crtFn = NULL;
 
         return true;
     }
     iTk=start;
     return false;
 }
+
 
 /*** block ::= instr+ ***/
 bool instr();
@@ -120,8 +181,19 @@ bool funcParams(){   //   a: int, b: real
 bool funcParam(){
     int start=iTk;
     if(consume(ID)){
+        const char *name = consumed->text;
+        Symbol *s = searchInCurrentDomain(name);
+        if(s) tkerr("symbol redefinition: %s", name);
+
+        s = addSymbol(name, KIND_ARG);
+        Symbol *sFnParam = addFnArg(crtFn, name);
+
         if(consume(COLON)){
-            if(baseType()) return true;
+            if(baseType()){
+                s->type = ret.type;
+                sFnParam->type = ret.type;
+                return true;
+            }
             else tkerr("missing basetype");
         }
         else tkerr("missing colon");
@@ -129,6 +201,7 @@ bool funcParam(){
     iTk=start;
     return false;
 }
+
 
 /*** instr ::= expr? SEMICOLON
     | IF LPAR expr RPAR block ( ELSE block )? END
@@ -279,6 +352,10 @@ bool factor(){  //punct de intrare pt expresii complexe
     }
 
     if(consume(ID)){
+        const char *name = consumed->text;
+        Symbol *s = searchSymbol(name); // CAUTĂ ÎN TOATE DOMENIILE
+        if(!s) tkerr("undefined symbol: %s", name);
+
         if(consume(LPAR)){
             if(expr()){
                 while(consume(COMMA)){
@@ -289,6 +366,7 @@ bool factor(){  //punct de intrare pt expresii complexe
         }
         return true;
     }
+
 
     iTk=start;
     return false;
